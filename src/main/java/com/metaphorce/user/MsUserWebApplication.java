b00c8367d198f5eef5875cba06c1d76b2519a9ed
Erick Antonio Reyes Montalvo <46572152+ErickMontalvo732@users.noreply.github.com>
2024-07-16 22:32:19 -0600

    Add files via upload

---
+++ src/main/java/com/metaphorce/user/MsUserWebApplication.java
@@ -0,0 +1,13 @@
+package com.metaphorce.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metaphorce/user/MsUserWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaphorce.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
